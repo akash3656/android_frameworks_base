fa557f58d633c5631f352f390d94dca881d742d8
Akash Ghosh <akash3656@gmail.com>
2015-01-30 01:13:55 +0800

    Revert "Keep quiet when volume keys are used to wake up device"
    This reverts commit e0fb35b9ed52d05ce74b1c1833e20af6cd649f3c.

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -363,11 +363,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mVolumeRockerWake;
     int mBackKillTimeout;
 
-    // During wakeup by volume keys, we still need to capture subsequent events
-    // until the key is released. This is required since the beep sound is produced
-    // post keypressed.
-    boolean mVolumeWakeTriggered;
-
     // Behavior of volbtn music controls
     boolean mVolBtnMusicControls;
     boolean mIsLongPress;
@@ -4382,17 +4377,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_VOLUME_DOWN:
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_MUTE: {
-                // Eat all down & up keys when using volume wake.
-                // This disables volume control, music control, and "beep" on key up.
-                if (isWakeKey && mVolumeRockerWake) {
-                    mVolumeWakeTriggered = true;
-                    break;
-                } else if (mVolumeWakeTriggered && !down) {
-                    result &= ~ACTION_PASS_TO_USER;
-                    mVolumeWakeTriggered = false;
-                    break;
-                }
-
                 if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
                     if (down) {
                         if (interactive && !mVolumeDownKeyTriggered
@@ -4454,7 +4438,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     }
                 }
 
-                if ((result & ACTION_PASS_TO_USER) == 0) {
+             // Disable music and volume control when used as wake key
+                if ((result & ACTION_PASS_TO_USER) == 0 && !mVolumeRockerWake) {
                     boolean mayChangeVolume = false;
 
                     if (isMusicActive()) {
